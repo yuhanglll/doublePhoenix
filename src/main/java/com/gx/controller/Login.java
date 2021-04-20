3d2ee1fe4197be323160aeaa30c70737c9440036
yuhanglll <2449068191>
2021-04-20 14:41:13 +0800

    idea上传

--- src/main/java/com/gx/controller/Login.java
+++ src/main/java/com/gx/controller/Login.java
@@ -12,11 +12,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class Login {
 
+    /**
+     * 跳转登录页
+     * @return
+     */
     @GetMapping("/doLogin")
     public Object doLogin() {
         return "/login";
     }
 
+    /**
+     * 登录接口
+     * @param username
+     * @param password
+     * @param model
+     * @return
+     */
     @PostMapping("/login")
     public Object login(String username, String password, Model model) {
         model.addAttribute("user", username);
